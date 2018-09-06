34eed1cdc6e6858d9b7ed4cbc0f7de5d7eca8f37
Artem Ivanov <fast0490f@mail.ru>
2018-09-06 13:58:42 +0300

    version 0.0.2

--- index.js
+++ index.js
@@ -2,18 +2,40 @@ const Plugin = require('./lib/plugin');
 const Telegram = require('./lib/telegram');
 
 const plugin = new Plugin();
+const users = {};
 let telegram = null;
+
+plugin.on('debug', mode => {});
 
 plugin.on('params', params => {
   start(params);
 });
 
-plugin.on('debug', mode => {
+plugin.on('infousers', list => {
+  list.forEach(item => {
+    users[Number(item.addr)] = item;
+  });
 });
 
+function checkUser(id) {
+  if (users.hasOwnProperty(id)) {
+    return users[id];
+  }
+  return null ;
+}
+
+function telegram_user_not_found(id) {
+  telegram.sendText(id, `Незарегистрированный пользователь ${id}. Руководство по настройке https://intrahouse.ru/shop/`);
+}
+
 function telegram_message({ from, text }) {
-  plugin.debug(`msg -> id:${from.id}, text:${text}`);
-  telegram.sendText(from.id, text);
+  const user = checkUser(from.id);
+  if (user) {
+    plugin.debug(`msg -> id:${from.id}, text:${text}`);
+    telegram.sendText(from.id, text);
+  } else {
+    telegram_user_not_found(from.id)
+  }
 }
 
 function telegram_debug(text) {
@@ -21,7 +43,7 @@ function telegram_debug(text) {
 }
 
 function start(options) {
-  plugin.debug("version: 0.0.1");
+  plugin.debug("version: 0.0.2");
 
   telegram = new Telegram({ token: options.token, proxy: options.proxy === 'manual' ? options.HTTPProxy : options.proxy });
   telegram.on('debug', telegram_debug);
