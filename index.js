5454ec957ca58f825b8ddbaf6f05097317fd60f2
Artem Ivanov <fast0490f@mail.ru>
2018-09-06 10:09:41 +0300

    basic messages

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const Plugin = require('./lib/plugin');
 const Telegram = require('./lib/telegram');
 
 const plugin = new Plugin();
+let telegram = null;
+let debug = false;
 
 plugin.on('params', params => {
   start(params);
 });
 
-function message(msg) {
-  console.log(msg);
+plugin.on('debug', mode => {
+  debug = mode;
+});
+
+function telegram_message({ from, text }) {
+  if (debug) {
+    plugin.debug(`msg: ${from.id}, ${text}`);
+  }
+  telegram.sendText(from.id, text);
+}
+
+function telegram_debug(text) {
+  if (debug) {
+    plugin.debug(text);
+  }
 }
 
 function start(options) {
-  const telegram = new Telegram({ token: options.token, proxy: 'auto' });
-  telegram.on('msg_text', message);
+  plugin.debug("version: 0.0.1");
+
+  telegram = new Telegram({ token: options.token, proxy: 'auto' });
+  telegram.on('debug', telegram_debug);
+  telegram.on('msg_text', telegram_message);
+
+  telegram.start();
 }
