98a2a35f4de22aeb114ae1a3dd1da1d0e973c435
Artem Ivanov <fast0490f@mail.ru>
2018-09-06 11:03:35 +0300

    add node modules fix bug

--- index.js
+++ index.js
@@ -3,27 +3,21 @@ const Telegram = require('./lib/telegram');
 
 const plugin = new Plugin();
 let telegram = null;
-let debug = false;
 
 plugin.on('params', params => {
   start(params);
 });
 
 plugin.on('debug', mode => {
-  debug = mode;
 });
 
 function telegram_message({ from, text }) {
-  if (debug) {
-    plugin.debug(`msg: ${from.id}, ${text}`);
-  }
+  plugin.debug(`msg: ${from.id}, ${text}`);
   telegram.sendText(from.id, text);
 }
 
 function telegram_debug(text) {
-  if (debug) {
-    plugin.debug(text);
-  }
+  plugin.debug(text);
 }
 
 function start(options) {
