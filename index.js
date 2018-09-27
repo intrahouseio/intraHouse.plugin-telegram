6553ed4882dd9060c6b9f283a11caf77c6389eab
Artem Ivanov <fast0490f@mail.ru>
2018-09-27 11:25:14 +0300

    version 0.0.7

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function telegram_debug(text) {
 }
 
 function start(options) {
-  plugin.debug("version: 0.0.6");
+  plugin.debug("version: 0.0.7");
 
   telegram = new Telegram({ token: options.token, proxy: options.proxy === 'manual' ? options.HTTPProxy : options.proxy });
   telegram.on('debug', telegram_debug);
