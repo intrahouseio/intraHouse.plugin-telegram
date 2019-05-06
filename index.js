68c1ced975d728dd266fe3606da14b4af88cfbc7
System Administrator <fast0490f@mail.ru>
2019-05-06 11:32:50 +0300

    version 0.0.15

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function telegram_message({ from, text }) {
   if (user) {
     plugin.debug(`msg -> id:${from.id}, text:${text}`);
     plugin.setChannelsData([{ id: 'incoming_message', value: text, ext: { userid: from.id, update: Date.now()} }])
-    plugin.setChannelsData([{ id: 'incoming_message', value: text + " ", ext: { userid: from.id, update: Date.now()} }])
+    plugin.setChannelsData([{ id: 'incoming_message', value: '#' + text, ext: { userid: from.id, update: Date.now()} }])
     // telegram.sendText(from.id, `Received: ${text}`);
   } else {
     telegram_user_not_found(from.id)
@@ -63,7 +63,7 @@ function telegram_debug(text) {
 }
 
 function start(options) {
-  plugin.debug("version: 0.0.14");
+  plugin.debug("version: 0.0.15");
 
   plugin.setChannels([{ id: 'incoming_message', desc: 'incoming_message' }]);
 
